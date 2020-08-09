4924024d1c28cb777ba87b52dd7a0025613cd5c2
Alexander Loshakov <alevas556@icloud.com>
2020-08-09 11:20:19 +0300

    Отображение данных на экране

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,6 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var numberFromScrean : Double = 0;
+    
+    @IBOutlet weak var result: UILabel!
+    
+    @IBAction func digits(_ sender: UIButton) {
+        result.text = result.text! + String(sender.tag)
+        numberFromScrean = Double(result.text!)!
+    }
+    
+    
+    @IBAction func buttons(_ sender: UIButton) {
+        if result.text != "" {
+            if sender.tag == 11 { // Деление
+                
+            }
+            else if sender.tag == 12 { // Умножение
+                
+            }
+            else if sender.tag == 13 { // Вычитание
+                
+            }
+            else if sender.tag == 14 { // Добавление
+                
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
