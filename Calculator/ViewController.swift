4bea2cb76edd1b5dd7993fec967f76405b804b99
Alexander Loshakov <alevas556@icloud.com>
2020-08-09 13:14:37 +0300

    Математические операции

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -11,29 +11,66 @@ import UIKit
 class ViewController: UIViewController {
 
     var numberFromScrean : Double = 0;
+    var firstNum : Double = 0;
+    var operation : Int = 0;
+    var mathSign : Bool = false;
     
     @IBOutlet weak var result: UILabel!
     
     @IBAction func digits(_ sender: UIButton) {
-        result.text = result.text! + String(sender.tag)
+        
+        if mathSign == true {
+            result.text = String(sender.tag)
+            mathSign = false
+        }
+        else {
+            result.text = result.text! + String(sender.tag)
+        }
+        
         numberFromScrean = Double(result.text!)!
     }
     
     
     @IBAction func buttons(_ sender: UIButton) {
-        if result.text != "" {
+        if result.text != "" && sender.tag != 10 && sender.tag != 15 {
+            if result.text != "÷" && result.text != "x" && result.text != "-" && result.text != "+" {
+                firstNum = Double(result.text!)!
+            }
             if sender.tag == 11 { // Деление
-                
+                result.text = "÷";
             }
             else if sender.tag == 12 { // Умножение
-                
+                result.text = "x";
             }
             else if sender.tag == 13 { // Вычитание
-                
+                result.text = "-";
             }
             else if sender.tag == 14 { // Добавление
-                
+                result.text = "+";
             }
+            
+            operation = sender.tag
+            mathSign = true;
+        }
+        else if sender.tag == 15 { // знак равно
+            if operation == 11 {
+                result.text = String(firstNum / numberFromScrean)
+            }
+            else if operation == 12 {
+                result.text = String(firstNum * numberFromScrean)
+            }
+            else if operation == 13 {
+                result.text = String(firstNum - numberFromScrean)
+            }
+            else if operation == 14 {
+                result.text = String(firstNum + numberFromScrean)
+            }
+        }
+        else if sender.tag == 10 { // очистить
+            result.text = ""
+            firstNum = 0;
+            numberFromScrean = 0;
+            operation = 0;
         }
     }
     
